628b4e026067845541961fb8134b45b3d0cbafe1
bhavanaboddapati <boddapatibhavana@gmail.com>
2025-10-12 19:32:20 +0530

    Initial commit: setup Spring Boot (Maven) project structure

---
+++ src/main/java/com/ecommerce/ecommerce/service/CategoryServiceImpl.java
@@ -0,0 +1,48 @@
+package com.ecommerce.ecommerce.service;
+
+import com.ecommerce.ecommerce.model.Category;
+import com.ecommerce.ecommerce.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+    //private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+
+        categoryRepository.delete(category);
+        return "Category with categoryId: " + categoryId + " deleted successfully !!";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Category savedCategory = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"));
+
+        category.setCategoryId(categoryId);
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
+    }
+}
