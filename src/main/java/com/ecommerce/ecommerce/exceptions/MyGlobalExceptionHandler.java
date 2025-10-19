1ce86f393870c51eea911c3df155b767a9190d2f
bhavanaboddapati <boddapatibhavana@gmail.com>
2025-10-19 07:05:12 +0530

    APIResponse object for uniform exception

--- src/main/java/com/ecommerce/ecommerce/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/ecommerce/exceptions/MyGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ecommerce.ecommerce.exceptions;
 
+import com.ecommerce.ecommerce.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -27,14 +28,16 @@ public class MyGlobalExceptionHandler {
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e) {
+    public ResponseEntity<APIResponse> myResourceNotFoundException(ResourceNotFoundException e) {
         String message = e.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        APIResponse response = new APIResponse(message, false);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(APIException.class)
-    public ResponseEntity<String> myAPIException(Exception e) {
+    public ResponseEntity<APIResponse> myAPIException(Exception e) {
         String message = e.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        APIResponse response = new APIResponse(message, false);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
